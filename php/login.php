d974d562f7810e00633577a1c69689257cd667c9
Carlos Daniel Martínez López <carlos.martinez.cbtis59@gmail.com>
2023-02-14 15:47:41 -0600

    reformateo de codigo de bd

--- php/login.php
+++ php/login.php
@@ -1,25 +1,23 @@
 <?php
     require_once 'conexion.php';
 
-    if(isset($_POST['btnIngresar'])){
-        $tsql = "SELECT * FROM dbo.Login WHERE usuario = ? AND contraseña = ?";
-        $params = array($_POST['usuario'], $_POST['contraseña']);
+    $tsql = "SELECT * FROM dbo.Login WHERE usuario = ? AND contraseña = ?";
+    $params = array($_POST['usuario'], $_POST['contraseña']);
 
-        $stmt = sqlsrv_query($conn, $tsql, $params);
+    $stmt = sqlsrv_query($conn, $tsql, $params);
 
-        if ($stmt === false) {
-            echo "Error en la consulta.<br>";
-            die(print_r(sqlsrv_errors(), true));
-        }
-
-     if (sqlsrv_has_rows($stmt)) {
-            header("Location: pagina_destino.html");
-            exit;
-        }
-    } else {
-        echo "Usuario o contraseña incorrectos.<br>";
+    if ($stmt === false) {
+        echo "Error en la consulta.<br>";
+        die(print_r(sqlsrv_errors(), true));
     }
 
+    if (sqlsrv_has_rows($stmt)) {
+        header("Location: pagina_destino.html");
+        exit;
+    }else {
+        echo "Usuario o contraseña incorrectos.<br>";
+    }
+    
     sqlsrv_free_stmt($stmt);
     sqlsrv_close($conn);
 ?>
